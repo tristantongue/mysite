652acb180fc74ff6f0d00a94a762c14cef5ac6fc
Tristan Tongue <tristantongue@gmail.com>
2020-11-02 09:44:40 -0600

    hw2 finalized bash script

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-cd docs
-cat 
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/experience.html templates/bottom.html > docs/experience.html
+cat templates/top.html content/musicpost.html templates/bottom.html > docs/musicpost.html
+cat templates/top.html content/schiller.html templates/bottom.html > docs/schiller.html
