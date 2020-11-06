0326352de458c98ecd7cb23ba38986c3ad683e7d
Tristan Tongue <tristantongue@gmail.com>
2020-11-06 16:09:48 -0600

    fixed python script to not append every time

--- build.py
+++ build.py
@@ -9,23 +9,23 @@ experience = open('content/experience.html').read()
 music = open('content/musicpost.html').read()
 schiller = open('content/schiller.html').read()
 
-open('docs/index.html', 'a+').write(top)
+open('docs/index.html', 'w').write(top)
 open('docs/index.html', 'a+').write(index)
 open('docs/index.html', 'a+').write(bottom)
 
-open('docs/about.html', 'a+').write(top)
+open('docs/about.html', 'w').write(top)
 open('docs/about.html', 'a+').write(about)
 open('docs/about.html', 'a+').write(bottom)
 
-open('docs/experience.html', 'a+').write(top)
+open('docs/experience.html', 'w').write(top)
 open('docs/experience.html', 'a+').write(experience)
 open('docs/experience.html', 'a+').write(bottom)
 
-open('docs/musicpost.html', 'a+').write(top)
+open('docs/musicpost.html', 'w').write(top)
 open('docs/musicpost.html', 'a+').write(music)
 open('docs/musicpost.html', 'a+').write(bottom)
 
-open('docs/schiller.html', 'a+').write(top)
+open('docs/schiller.html', 'w').write(top)
 open('docs/schiller.html', 'a+').write(schiller)
 open('docs/schiller.html', 'a+').write(bottom)
 
