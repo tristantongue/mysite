bfd4b74f8aaa718c7581490a4b2f42cdf45bc6aa
Tristan Tongue <tristantongue@gmail.com>
2020-11-09 11:01:24 -0600

    KS CODING HW3, ssg script and base template

--- build.py
+++ build.py
@@ -1,32 +1,54 @@
-print('building static site')
+pages = [
+        {
+            "filename": "content/index.html",
+            "output": "docs/index.html",
+            "title": "Tristan Tongue",
+        },
+        {
+            "filename": "content/about.html",
+            "output": "docs/about.html",
+            "title": "About Me",
+        },
+        {
+            "filename": "content/experience.html",
+            "output": "docs/experience.html",
+            "title": "Experience - Tristan Tongue",
+        },
+        {
+            "filename": "content/musicpost.html",
+            "output": "docs/musicpost.html",
+            "title": "The Mathematical Relationships of Music",
+        },
+        {
+            "filename": "content/schiller.html",
+            "output": "docs/schiller.html",
+            "title": "Aesthetics in Schiller and Schopenhauer",
+        },
+    ]
+#build template function reads in base template, replaces the title palceholder with the arg, and returns the ready to be used template
+def build_template(pagetitle):
+    template = open('templates/base.html').read()
+    ready_template = template.replace("{{title}}", pagetitle)
+    return ready_template
+#build page function take the args of template and content, and replaces the content placeholder with a content page
+def build_page(template, content):
+    finished_page = template.replace("{{content}}", content)
+    return finished_page
 
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
+def main():
+    print('building static site')
 
-index = open('content/index.html').read()
-about = open('content/about.html').read()
-experience = open('content/experience.html').read()
-music = open('content/musicpost.html').read()
-schiller = open('content/schiller.html').read()
+    
+    for page in pages:
+        page_content = page['filename']
+        page_title = page['title']
+        page_output = page['output']
 
-open('docs/index.html', 'w').write(top)
-open('docs/index.html', 'a+').write(index)
-open('docs/index.html', 'a+').write(bottom)
-
-open('docs/about.html', 'w').write(top)
-open('docs/about.html', 'a+').write(about)
-open('docs/about.html', 'a+').write(bottom)
-
-open('docs/experience.html', 'w').write(top)
-open('docs/experience.html', 'a+').write(experience)
-open('docs/experience.html', 'a+').write(bottom)
-
-open('docs/musicpost.html', 'w').write(top)
-open('docs/musicpost.html', 'a+').write(music)
-open('docs/musicpost.html', 'a+').write(bottom)
-
-open('docs/schiller.html', 'w').write(top)
-open('docs/schiller.html', 'a+').write(schiller)
-open('docs/schiller.html', 'a+').write(bottom)
+        template = build_template(page_title)
+        content = page_content.read()
+        finished_page = build_page(template, content)
+        open(page_output, "w+").write(finished_page)
 
 
+
+main()
